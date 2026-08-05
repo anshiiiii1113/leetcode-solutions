3c5a362627f7405b55152688f06595951c131beb
Anshita Gupta <anshita13g@gmail.com>
2026-08-05 20:28:07 +0530

    Time: 4 ms (82.68%) | Memory: 46.5 MB (34.86%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        char[] sCh = s.toCharArray();
+        char[] tCh = t.toCharArray();
+        
+        Arrays.sort(sCh);
+        Arrays.sort(tCh);
+        
+        return Arrays.equals(sCh, tCh);
+    
+}
+    
+}
